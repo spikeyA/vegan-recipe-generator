a03eabb4417f7c1b777084c252bb5727c0d53275
spikey <mail2.shastry@gmail.com>
2025-05-24 13:34:41 -0600

    update changes to UI and recipe links

---
+++ src/pages/VeganChatbot.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VoiceChatBot from '../components/VoiceChatBot'; // Adjust path if needed
+
+function VeganChatbot() {
+  return (
+    <div>
+      <VoiceChatBot />
+    </div>
+  );
+}
+
+export default VeganChatbot;
